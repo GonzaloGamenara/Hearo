9799b8622e99d05a7b7ab754e4d12b85805b01cd
Gonzalo Gamenara <103406044+GonzaloGamenara@users.noreply.github.com>
2025-08-06 04:39:32 +0000

    parte2

--- client/src/routes/AutoRedirect.jsx
+++ client/src/routes/AutoRedirect.jsx
@@ -4,6 +4,11 @@ import { useAuth } from "../hooks/useAuth";
 export const AutoRedirect = () => {
   const { isAuthenticated } = useAuth();
 
-  if (isAuthenticated === null) return <p>Cargando...</p>;
+  if (isAuthenticated === null)
+    return (
+      <div>
+        <h2>Cargando...</h2>
+      </div>
+    );
   return isAuthenticated ? <Navigate to="/home" /> : <Navigate to="/login" />;
 };
--- server/middlewares/authorization.js
+++ server/middlewares/authorization.js
@@ -1,11 +1,11 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import User from "../models/User.js";
 
 dotenv.config();
 
-export function onlyLogged(req, res, next) {
+export async function onlyLogged(req, res, next) {
   const token = req.cookies.jwt;
-  console.log("Token recibido:", token);
 
   if (!token) {
     return res.status(401).json({ message: "No autorizado (sin token)" });
@@ -13,9 +13,14 @@ export function onlyLogged(req, res, next) {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log("Token decodificado:", decoded);
-    req.user = decoded; // para checkAuth u otras rutas
-    next();
+    console.log("usuario decodificado", decoded);
+    const usuarioExistente = await User.findOne({ user: decoded.user });
+    if (!usuarioExistente) {
+      return res
+        .status(400)
+        .send({ status: "Error", message: "Usuario no existente!" });
+    }
+    return res.status(200).json({ status: "ok", user: usuarioExistente });
   } catch (error) {
     console.error("Error al verificar el token:", error.message);
     return res.status(401).json({ message: "Token inválido" });
